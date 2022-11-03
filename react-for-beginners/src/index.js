bcc027b39409a3142042c2422297a0b97fc4878e
KIMTAEIL <caporatang@naver.com>
2022-11-04 04:55:03 +0900

    useEffect example

--- react-for-beginners/src/index.js
+++ react-for-beginners/src/index.js
@@ -4,9 +4,7 @@ import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+root.render( 
     <App />
-  </React.StrictMode>
 );
 
