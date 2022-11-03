a7fc3127b2746b5d2d8482e163ee587ca63d97cf
KIMTAEIL <caporatang@naver.com>
2022-11-04 04:34:38 +0900

    useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,13 +1,18 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-
-
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1)
+  console.log("i run all the time");
+
+  //useEffect는 최초 렌더될때 한번만 실행 시켜줌
+  useEffect(() => {
+    console.log("CALL THE API.");
+  }, []);
   return (
    <div>
-      <h1 className={styles.title}>Welcome back!!!!</h1>        
-      <Button text={"Continue"}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>        
     </div>
   );
 }
